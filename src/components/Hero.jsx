ace2cdfc39c0a845070182f8ecd44e82cd37fad8
rajat-k-27 <chakrabortyrajat3689@gmail.com>
2024-10-23 21:06:46 +0530

    updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px] max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
@@ -19,15 +19,16 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Rajat</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals <br className='sm:block hidden' />
-            mobile and web applications
+            I develop 3D visuals, mobile <br className='sm:block hidden' />
+            and web applications
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
-      <div className='absolute xs:bottom-1 bottom-20 w-full flex justify-center items-center'>
-        <a href='#about' aria-label="scroll">
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div
               animate={{
@@ -43,11 +44,7 @@ const Hero = () => {
           </div>
         </a>
       </div>
-
-      
     </section>
-
-    
   );
 };
 
